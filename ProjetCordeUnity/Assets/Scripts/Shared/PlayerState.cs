aac530ffbeba1f60908725157967034f5985ecb5
Louis Guillemin <louisguillemin@msn.com>
2014-11-05 17:40:33 +0100

    Committed basic scene with basic scripts: idol detects barycenter of group of players and positions itself on it. Can remove players from the linkage for 5 seconds with input.

---
+++ ProjetCordeUnity/Assets/Scripts/Shared/PlayerState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerState : MonoBehaviour {
+
+	public bool v_isLinked;
+	private float _linkingTimer;
+
+	// Use this for initialization
+	void Start () {
+		_linkingTimer=5f;
+		v_isLinked=true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(v_isLinked==false){
+			_linkingTimer -= Time.deltaTime;
+		}
+
+		if(_linkingTimer<=0){
+			_linkingTimer=5;
+			v_isLinked=true;
+
+		}
+	}
+}
